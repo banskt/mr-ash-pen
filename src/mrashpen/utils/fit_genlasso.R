9396330ba24604bb5a0221ca6b99fc09005298a5
banskt <bnrj.saikat@gmail.com>
2022-02-15 17:09:35 -0600

    Use trendfilter instead of genlasso

--- src/mrashpen/utils/fit_genlasso.R
+++ src/mrashpen/utils/fit_genlasso.R
@@ -10,53 +10,60 @@ options = list(
   make_option("--nfolds", type="integer", default = 10,
               help="number of CV folds", metavar="int"),
   make_option("--order", type="integer", default = 1,
-              help="trendfiltering order", metavar="int")
+              help="trendfiltering order", metavar="int"),
+  make_option("--trendfilter", type="logical", action="store_true", default=FALSE,
+              help="fit using trendfilter")
 )
 
 opt_parser = OptionParser(option_list=options);
 opt = parse_args(opt_parser);
 data = readRDS(opt$infile);
 
-fit_trendfilter <- function (X, y, order = 1, nfolds = 5, cvlambda = "1se") {
+fit_trendfilter <- function (y, order = 1, nfolds = 5, cvlambda = "1se") {
     #pos   <- 1:length(y)
     #out   <- genlasso::trendfilter(y, pos, X, ord = order)
     out   <- genlasso::trendfilter(y, ord = order)
     cvout <- genlasso::cv.trendfilter(out, k = nfolds)
-    cvs   <- if (cvlambda == "1se") cvout$lambda.1se else cvout$lambda.min
-    b     <- as.vector(coef(out, lambda = cvs))
-    idx   <- which(out$lambda == cvs)
-    ypred <- out$fit[, idx]
-    return (list(fit = out, cv = cvout, mu = 0, beta = b$beta, ypred = ypred))
+    cvlam <- if (cvlambda == "1se") cvout$lambda.1se else cvout$lambda.min
+    cvidx <- if (cvlambda == "1se") cvout$i.1se else cvout$i.min
+    b     <- coef(out, lambda = cvlam)
+    #b    <- out$beta[,cvidx]
+    ypred <- out$fit[, cvidx]
+    return (list(fit = out, cv = cvout, mu = 0, 
+                 beta = as.vector(b$beta), ypred = ypred, df = b$df))
 }
 
 gcv_genlasso <- function(object) {
     lams <- object$lambda
-    df <- object$df
-    n <- length(object$y)
-    ymat <- matrix(object$y,n,length(lams))
+    df   <- object$df
+    n    <- length(object$y)
+    ymat <- matrix(object$y, n, length(lams))
     pred <- object$fit
-    err <- colMeans((ymat-pred)^2)/(1-df/n)^2
+    err  <- colMeans((ymat-pred)^2)/(1-df/n)^2
     names(err) <- round(lams,3)
     lam_min <- lams[which.min(err)]
-    out <- list(err = err, lambda = lams, lambda.min = lam_min)
+    out <- list(err = err, lambda = lams, lambda.min = lam_min, i.min = which(lams == lam_min))
     class(out) <- c("gcv.genlasso", "list")
     return (out)
 }
 
 
-fit_genlasso <- function (X, y, order = 1) {
-    D     <- genlasso::getDtf(length(y), order)
-    out   <- genlasso::genlasso(y = y, X = X, D = D)
+fit_genlasso <- function (y, order = 1) {
+    n     <- length(y)
+    D     <- genlasso::getDtf(n, order)
+    out   <- genlasso::genlasso(y = y, X = diag(n), D = D)
     cvout <- gcv_genlasso(out)
-    #cvs   <- cvout$lambda.min
-    tfout <- fit_trendfilter(X, y, order = order)
-    cvs   <- tfout$cv$lambda.1se
-    b     <- as.vector(coef(out, lambda = cvs))
-    idx   <- which(out$lambda == cvs)
-    ypred <- out$fit[, idx]
-    return (list(fit = out, cv = cvout, mu = 0, beta = b$beta, ypred = ypred))
+    cvidx <- cvout$i.min
+    cvlam <- cvout$lambda.min
+    b     <- coef(out, lambda = cvlam)
+    ypred <- out$fit[, cvidx]
+    return (list(fit = out, cv = cvout, mu = 0, 
+                 beta = as.vector(b$beta), ypred = ypred, df = b$df))
 }
 
-#res = fit_trendfilter(as.matrix(data$X), as.vector(data$y), order = opt$order, nfolds = opt$nfolds)
-res = fit_genlasso(as.matrix(data$X), as.vector(data$y), order = opt$order)
+if (opt$trendfilter) {
+    res = fit_trendfilter(as.vector(data$y), order = opt$order, nfolds = opt$nfolds)
+} else {
+    res = fit_genlasso(as.matrix(data$X), as.vector(data$y), order = opt$order)
+}
 saveRDS(res, file = opt$outfile)
