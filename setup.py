38e520f42fa327fbe0205b430208ea1573a73e7a
banskt <bnrj.saikat@gmail.com>
2022-04-18 00:08:06 -0500

    Exclude blas info for f2py modules in setup
    Numpy f2py automatically adds the blas info, hence it is not required to
    include the blas info separately.

--- setup.py
+++ setup.py
@@ -190,11 +190,11 @@ def ext_modules():
         extra_compile_args = ['-O3', '-Wall', '-fbounds-check', '-g', '-Wno-uninitialized', '-fno-automatic', '-ffast-math']
         #
         flibs       = cfg_lib_todict(flib_cfgs, flib_dir, libprefix)
-        #fmodules    = [compile_extension_dict(k, v, extra_libraries, extra_compile_args, \
-        #                  **dict(extra_info = blas_info)) \
-        #                  for k, v in flibs.items()]
-        fmodules    = [compile_extension_dict(k, v, extra_libraries, extra_compile_args)  \
-                           for k, v in flibs.items()]
+        fmodules    = [compile_extension_dict(k, v, extra_libraries, extra_compile_args, \
+                          **dict(extra_info = blas_info)) \
+                          for k, v in flibs.items()]
+        #fmodules    = [compile_extension_dict(k, v, extra_libraries, extra_compile_args)  \
+        #                   for k, v in flibs.items()]
     return cmodules + fmodules
 
 '''
